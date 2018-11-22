da63745859c34528c37ebe210a7a31219690e874
Stucki <loganstucki@gmail.com>
2018-11-22 15:31:41 -0600

    import helmet

--- src/components/blogPost.js
+++ src/components/blogPost.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Layout from "./layout";
+import Helmet from 'react-helmet'
 
 export default function Template({
   data, // this prop will be injected by the GraphQL query below.
