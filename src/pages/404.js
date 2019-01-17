bd4450cfa0cf52607bbcdf708ca489a95287615d
Stucki <loganstucki@gmail.com>
2019-01-17 12:25:09 -0600

    change 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,8 +3,13 @@ import Layout from '../components/layout'
 
 const NotFoundPage = () => (
   <Layout>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1>Akward...</h1>
+    <p>So that page doesn&#39;t exist... This is akward.</p>
+    <p>
+      If you clicked a link that brought you here could you email me so I can fix it?  
+    </p>
+    <p>K thanks bye.</p>
+    <h3>&lt;3</h3>
   </Layout>
 )
 
