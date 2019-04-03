9aa0f6a6a6011b82f924ed3f7f38bb5019b0114c
Stucki <loganstucki@gmail.com>
2019-04-03 11:53:59 -0500

    remove rss feed link

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -5,11 +5,11 @@ import { Link, graphql } from 'gatsby';
 
 const IndexPage = ({ data }) => (
   <Layout title="Blog">
-    <Link
+    {/*<Link
       to="/blog/rss.xml"
     >
       Subscribe with rss
-    </Link>
+    </Link>*/}
     <h1>Recent Posts</h1>
     {data.allMarkdownRemark.edges.map(({ node }) => (
       <>
